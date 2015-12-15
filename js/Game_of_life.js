48997f911a8f4a51b3fb047fd5f7d6b4301f26bb
Haoyu Wang <2411690987@qq.com>
2015-12-15 10:04:58 +0800

    完成了生命游戏的基本功能
    代码有待重构

--- js/Game_of_life.js
+++ js/Game_of_life.js
@@ -7,6 +7,7 @@ var Game_of_life = (function(){
     var DEAD = false;
     var TURN_SPEED = 500; //ms
     var cells = new Array(Y_MAX*X_MAX);
+    var aaa = {};
     var canvas;
     var pen;
 
@@ -39,8 +40,8 @@ var Game_of_life = (function(){
 
     //判断cell的生死
     function is_live(){
-        for(i in cells){
-            var j=0;
+        for(var i= 0; i<cells.length;i++){
+            var j = 0;
             if(cells[i-X_MAX-1] == LIVE){
                 j++
             }
@@ -62,16 +63,26 @@ var Game_of_life = (function(){
             if(cells[i+X_MAX] == LIVE){
                 j++
             }
-            if(cells[i-X_MAX+1] == LIVE){
+            if(cells[i+X_MAX+1] == LIVE){
                 j++
             }
 
-            if(!(j == 2 || j == 3) && cells[i] == LIVE){
-                cells[i] = DEAD;
+            if(j == 3 && cells[i] == DEAD){
+                aaa[i] = LIVE;
             }
 
-            if(j == 3 && cells[i] == DEAD){
-                cells[i] = LIVE;
+            if((j< 2 || j > 3) && cells[i] == LIVE){
+                aaa[i] = DEAD;
+            }
+        }
+    }
+
+    function bbb(){
+        for(var i= 0; i<cells.length;i++){
+            for(name in aaa){
+                if(i == name){
+                    cells[i] = aaa[name];
+                }
             }
         }
     }
@@ -95,7 +106,20 @@ var Game_of_life = (function(){
         cells[125]= LIVE;
         cells[126]= LIVE;
         cells[127]= LIVE;
+
+        cells[255]= LIVE;
+        cells[253]= LIVE;
+        cells[285]= LIVE;
+        cells[225]= LIVE;
+        cells[284]= LIVE;
+
+        cells[355]= LIVE;
+        cells[353]= LIVE;
+        cells[385]= LIVE;
+        cells[325]= LIVE;
+        cells[354]= LIVE;
     }
+
 
     //渲染画面
     function render(){
@@ -111,6 +135,8 @@ var Game_of_life = (function(){
     function main_loop(){
         render();
         is_live();
+        bbb();
+        alert("a");
         setTimeout(main_loop,TURN_SPEED);
     }
 
